340f5b545158d0a0879ad91bbb15e87684093fbe
Baskar <baskar2294@gmail.com>
2018-02-12 15:09:33 +0530

    kil

--- git demo/Program.cs
+++ git demo/Program.cs
@@ -10,7 +10,7 @@ namespace git_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world edit removed");
+            Console.WriteLine("hello world edit removed by baskar");
         }
     }
 }
